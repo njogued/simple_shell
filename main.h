d10e08246c46f7e8c0df2e00045effc66c359f66
Wambani01 <georgewmbn@gmail.com>
2022-11-14 02:27:56 -0800

    getline function

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+extern char **environ;
+
+
+
+
+#endif 
